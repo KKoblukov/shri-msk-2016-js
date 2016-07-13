b4dc9db0e6c8165a884186a2d6ccdcbd9c84a28f
Azat S <to@azat.io>
2016-07-13 23:02:22 +0300

    Create first door

--- src/js/doorOthers.js
+++ src/js/doorOthers.js
@@ -5,7 +5,6 @@
  * @param {Number} number
  * @param {Function} onUnlock
  */
-
 function Door0(number, onUnlock) {
     DoorBase.apply(this, arguments);
 
@@ -65,11 +64,88 @@ function Door1(number, onUnlock) {
 
     // ==== Напишите свой код для открытия второй двери здесь ====
     // Для примера дверь откроется просто по клику на неё
-    this.popup.addEventListener('click', function () {
-        this.unlock();
+
+    var keys = [
+        this.popup.querySelector('.door-riddle__key_yellow'),
+        this.popup.querySelector('.door-riddle__key_green'),
+    ];
+
+    keys.forEach(function (k) {
+        k.addEventListener('pointerdown', _onKeyPointerDown.bind(this));
+        k.addEventListener('pointermove', _onKeyPointerMove.bind(this));
+        k.addEventListener('pointerup', _onKeyPointerUp.bind(this));
+        k.addEventListener('pointercancel', _onKeyPointerUp.bind(this));
+        k.addEventListener('pointerleave', _onKeyPointerUp.bind(this));
     }.bind(this));
+
+    function _onKeyPointerDown(e) {
+        e.target.classList.add('door-riddle__key_pressed');
+        moveAt(e);
+    }
+
+    function _onKeyPointerMove(e) {
+        moveAt(e);
+    }
+
+    function moveAt(e) {
+        if (e.target.classList.contains('door-riddle__key_pressed')) {
+            e.target.style.left = e.pageX - e.target.offsetWidth / 2 + 'px';
+            e.target.style.top = e.pageY - e.target.offsetHeight / 2 + 'px';
+        }
+    }
+
+    function _onKeyPointerUp(e) {
+        if (e.target.classList.contains('door-riddle__key_pressed')) {
+            e.target.classList.remove('door-riddle__key_pressed');
+        }
+
+        if (doElsCollide(greenKey, greenLock, 1, 1) === true) {
+            greenKey.classList.add('door-riddle__key_unlocked');
+            greenLock.style.display = 'none';
+        }
+
+        if (doElsCollide(yellowKey, yellowLock, 1, 1) === true) {
+            yellowKey.classList.add('door-riddle__key_unlocked');
+            yellowLock.style.display = 'none';
+        }
+        checkCondition.apply(this);
+    }
+
+    doElsCollide = function (el1, el2, c1, c2) {
+        var bodyRect = document.body.getBoundingClientRect();
+        var elemRect1 = el1.getBoundingClientRect();
+        var elemRect2 = el2.getBoundingClientRect();
+        el1.offsetBottom = elemRect1.top - bodyRect.top + c1 * el1.offsetHeight;
+        el1.offsetRight = elemRect1.left - bodyRect.left + c1 * el1.offsetWidth;
+        el2.offsetBottom = elemRect2.top - bodyRect.top + c2 * el2.offsetHeight;
+        el2.offsetRight = elemRect2.left - bodyRect.left + c2 * el2.offsetWidth;
+
+        return !((el1.offsetBottom < elemRect2.top - bodyRect.top) ||
+                 (elemRect1.top - bodyRect.top > el2.offsetBottom) ||
+                 (el1.offsetRight < elemRect2.left - bodyRect.left) ||
+                 (elemRect1.left - bodyRect.left > el2.offsetRight));
+    };
+
+    var greenKey = document.querySelector('.door-riddle__key_green');
+    var greenLock = document.querySelector('.door-riddle__lock_green');
+    var yellowKey = document.querySelector('.door-riddle__key_yellow');
+    var yellowLock = document.querySelector('.door-riddle__lock_yellow');
+
+    function checkCondition() {
+        var isOpened = true;
+        keys.forEach(function (k) {
+            if (!k.classList.contains('door-riddle__key_unlocked')) {
+                isOpened = false;
+            }
+        });
+
+        if (isOpened) {
+            this.unlock();
+        }
+    }
     // ==== END Напишите свой код для открытия второй двери здесь ====
 }
+
 Door1.prototype = Object.create(DoorBase.prototype);
 Door1.prototype.constructor = DoorBase;
 
